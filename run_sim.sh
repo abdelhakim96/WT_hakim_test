e010a8b5f3b2f21ebfabd657d7b29627deb4d467
Hakim <abdelhakim@aucegypt.edu>
2022-06-12 16:36:42 +0200

    installation script

--- run_sim.sh
+++ run_sim.sh
@@ -14,21 +14,6 @@ git clone git@github.com:abdelhakim96/WT_hakim_test.git
 
 
 
-
-
-# Download mavros dependecies
-
-echo "========================================="
-echo "Downloading mavros dependecies"
-echo "========================================="
-sudo apt-get install ros-melodic-mavros*
-sudo apt-get install xdotool
-sudo apt-get ros-mavros-mav-msgs 
-
-
-
-
-
 # copy PX4 folder to home directory
 echo "========================================="
 echo "copying PX4 folder to home"
@@ -54,9 +39,6 @@ cd ~/catkin_ws/src/WTI_catkin
 rm -r NMPC_code_generator
 rm -r wind_estimator
 
-
-
-
 # build catkin ws
 echo "========================================="
 echo "building ws"
@@ -64,30 +46,4 @@ echo "========================================="
 cd ~/catkin_ws/
 catkin clean
 catkin build
-
-
-
-# install_dependencies_and_setup_px4
-
-
-echo "========================================="
-echo "installing dependencies and setting up px4"
-echo "========================================="
-cd
-cd Wind-Turbine-Inspection
-./install_dependencies_and_setup_px4_modified.sh
-
-#Add alias for arming the drone and setting the mode to offboard.
-
-
-echo "========================================="
-echo "Adding alias for arming the drone and setting the mode to offboard.
-"
-echo "========================================="
-
-echo 'alias arm='rosrun mavros mavsafety arm'' >> ~/.bashrc 
-echo 'alias disarm='rosrun mavros mavsafety disarm'' >> ~/.bashrc 
-echo 'alias offboard='rosrun mavros mavsys mode -c OFFBOARD'' >> ~/.bashrc 
-
-
 
